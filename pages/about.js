a3add4701b1fa65291c77be2a34e5fc53a6e133b
Davide <dmolino@twistbioscience.com>
2022-07-29 17:55:58 -0700

    init styling about and navbar creation

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const About = () => {
     return (
-        <div className="">
+        <div className="bg-red-200 w-max my-0 mx-auto">
             <h2 className="">Who am I?</h2>
             <p>I&apos;m a Full Stack Software Engineer located in Los Angeles, CA.</p>
             <p>Passionate about contributing to open-source projects, application testing, and technology.</p>
